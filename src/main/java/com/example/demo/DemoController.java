e093de4d866e1220bee314dd1f8d12346aad1396
admin <dkim816@gmail.com>
2025-09-17 14:06:47 +0900

    lot's of edit and add my homepage

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -3,14 +3,20 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 
 @Controller
 public class DemoController {
-    @GetMapping("/hello")
+    @GetMapping("/hello1234")
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";
     }
+    @GetMapping("/about_detailed")
+    public String about() {
+        return "about_detailed";
+    }
+    
 }
