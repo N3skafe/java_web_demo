3383ff4c7b5104a12f4177d21fcc9096d77534e9
admin <dkim816@gmail.com>
2025-09-18 13:58:00 +0900

    W2_ultimate hello page loop

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -9,10 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DemoController {
-    @GetMapping("/hello1234")
-    public String hello(Model model) {
-        model.addAttribute("data", "hello!!");
-        return "hello1234";
+    @GetMapping("/hello1")
+    public String hello1() {
+        return "hello1";
+    }
+    @GetMapping("/hello2")
+    public String hello2() {
+        return "hello2";
     }
     @GetMapping("/about_detailed")
     public String about() {
